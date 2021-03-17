749dec4cf5a90ff011066d1700e5b8bf1611fdf4
DustinOlsen <69872283+DustinOlsen@users.noreply.github.com>
2021-03-17 12:39:19 -0500

    HWS Project 13 - Day 62
    ActionSheet

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,29 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount: CGFloat = 0 {
-        didSet {
-            print("new value is \(blurAmount)")
-        }
-    }
+    @State private var showingActionSheet = false
+    @State private var backgroundColor = Color.white
     var body: some View {
-        let blur = Binding<CGFloat>(
-            get: {
-                self.blurAmount
-            },
-            set: {
-                self.blurAmount = $0
-                print("New value is \(self.blurAmount)")
-            }
-        )
         
-        return VStack {
-            Text("I bring tidings")
-                .blur(radius: blurAmount)
-            
-            Slider(value: blur, in: 0...20)
-        }
-            
+        Text("Hello")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                self.showingActionSheet = true
+            }
+            .actionSheet(isPresented: $showingActionSheet) {
+                ActionSheet(title: Text("Change Background"), message: Text("Select a new color"), buttons: [
+                                .default(Text("Red")) { self.backgroundColor = .red },
+                                .default(Text("Green")) { self.backgroundColor = .green },
+                                .default(Text("Blue")) { self.backgroundColor = .blue },
+                                .cancel()
+                ])
+            }
     }
 }
 
