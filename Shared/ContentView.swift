a530f02cb1fd6c4b5a8b539399936c6145a0fc91
DustinOlsen <69872283+DustinOlsen@users.noreply.github.com>
2021-03-17 12:32:33 -0500

    HWS Day Project 13 - Day 62
    Custom Bindings

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount: CGFloat = 0 {
+        didSet {
+            print("new value is \(blurAmount)")
+        }
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        let blur = Binding<CGFloat>(
+            get: {
+                self.blurAmount
+            },
+            set: {
+                self.blurAmount = $0
+                print("New value is \(self.blurAmount)")
+            }
+        )
+        
+        return VStack {
+            Text("I bring tidings")
+                .blur(radius: blurAmount)
+            
+            Slider(value: blur, in: 0...20)
+        }
+            
     }
 }
 
