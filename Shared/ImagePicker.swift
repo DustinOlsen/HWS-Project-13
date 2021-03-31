5d208d01a7959f91e9d903fb0c60e7cf135ef644
DustinOlsen <69872283+DustinOlsen@users.noreply.github.com>
2021-03-31 12:32:34 -0500

    HWS Project 13 Day 64 Part 2

---
+++ Shared/ImagePicker.swift
@@ -0,0 +1,21 @@
+//
+//  ImagePicker.swift
+//  HWS Project 13
+//
+//  Created by Dustin Olsen on 3/31/21.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    typealias UIViewControllerType = UIImagePickerController
+}
